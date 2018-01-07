8fdc73309ed13aa486fa42789371262472ecba77
Michaelwolf95 <michaelwolf95@gmail.com>
2018-01-07 14:01:21 -0800

    Started work on character system

---
+++ SamuraiGame/Assets/_Scripts/PlamakerUnits AI/StateActions/Move2DCharacter.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HutongGames.PlayMaker;
+using UnityStandardAssets._2D;
+
+public class Move2DCharacter : FsmStateAction
+{
+    //[RequiredField]
+   // [CheckForComponent(typeof(Animator))]
+   // [Tooltip("The target. An Animator component is required")]
+    public FsmOwnerDefault gameObject;
+    private PlatformerCharacter2D _character;
+
+    public override void Reset()
+    {
+        gameObject = null;
+        _character = null;
+    }
+
+    public override void OnEnter()
+    {
+        // get the animator component
+        var go = Fsm.GetOwnerDefaultTarget(gameObject);
+
+        if (go == null)
+        {
+            Finish();
+            return;
+        }
+
+        _character = go.GetComponent<PlatformerCharacter2D>();
+
+        //MoveCharacter();
+    }
+
+    public override void OnFixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        //To Do: Call Move
+    }
+
+}
