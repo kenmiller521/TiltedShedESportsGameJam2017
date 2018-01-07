499747620fa15a6960bf036be24e31410d989acc
Michaelwolf95 <michaelwolf95@gmail.com>
2018-01-07 15:54:54 -0800

    Got basic unit move and attack working.

---
+++ SamuraiGame/Assets/_Scripts/UnitSystem/PlamakerUnits AI/RaycastUnitChecker.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastUnitChecker : MonoBehaviour
+{
+    [SerializeField] private Unit _unit;
+    [SerializeField] private int _ownerNumber;
+    public LayerMask CheckMask;
+	void Start ()
+    {
+        if (!_unit) _unit = this.GetComponentInParent<Unit>();
+        if(_unit)
+        {
+            _ownerNumber = _unit.OwnerNumber;
+        }
+	}
+
+    public bool CheckUnits(out GameObject resultGameObject, out float resultDistance, float checkDist)
+    {
+        RaycastHit2D[] hits;
+        hits = Physics2D.LinecastAll(this.transform.position, this.transform.position + this.transform.right * checkDist, CheckMask.value);
+        if (hits.Length > 0)
+        {
+            foreach (var h in hits)
+            {
+                var go = (h.collider.attachedRigidbody) ? h.collider.attachedRigidbody.gameObject : h.collider.gameObject;
+                Unit unit = go.GetComponent<Unit>();
+                if (unit)
+                {
+                    if (unit.OwnerNumber != _ownerNumber)
+                    {
+                        // NOTE: This doesn't guaruntee its the closest.
+                        resultGameObject = go;
+                        resultDistance = h.distance;
+                        return true;
+                    }
+                }
+            }
+        }
+        else
+        {
+            Debug.Log("LineCast Empty");
+        }
+        resultGameObject = null;
+        resultDistance = float.MaxValue;
+        return false;
+    }
+	
+}
---
+++ SamuraiGame/Assets/_Scripts/UnitSystem/PlamakerUnits AI/StateActions/CheckForClosestEnemyUnit.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HutongGames.PlayMaker;
+
+public class CheckForClosestEnemyUnit : FsmStateAction
+{
+    public FsmOwnerDefault raycastGameObject;
+    public FsmFloat _checkDistance = 5f;
+
+    public FsmGameObject _closestEnemyUnitVar;
+    public FsmFloat _distanceVar;
+
+    private RaycastUnitChecker _unitChecker;
+
+    public override void Awake()
+    {
+        base.Awake();
+        Fsm.HandleFixedUpdate = true;
+    }
+    public override void Reset()
+    {
+        raycastGameObject = null;
+        _unitChecker = null;
+    }
+
+    public override void OnEnter()
+    {
+        var go = Fsm.GetOwnerDefaultTarget(raycastGameObject);
+
+        if (go == null)
+        {
+            Debug.Log("Finishing State");
+            Finish();
+            return;
+        }
+        _unitChecker = go.GetComponent<RaycastUnitChecker>();
+        if(_unitChecker)
+        {
+            Debug.Log("Found Unit Checker");
+        }
+        //_raycastTransform = go.transform;
+    }
+    public override void OnFixedUpdate()
+    {
+        //Debug.Log("FixedUpdate State");
+        //RaycastCheckForEnemyUnits();
+        RaycastCheck();
+    }
+    private void RaycastCheck()
+    {
+        if(_unitChecker)
+        {
+            GameObject resultObj = null;
+            float resultDist;
+            if(_unitChecker.CheckUnits(out resultObj, out resultDist, _checkDistance.Value))
+            {
+                _closestEnemyUnitVar.Value = resultObj;
+                _distanceVar.Value = resultDist;
+                Debug.Log("FOUND CLOSEST");
+            }
+        }
+    }
+    //private void RaycastCheckForEnemyUnits()
+    //{
+    //    //RaycastHit2D hit;
+    //    //hit = Physics2D.Linecast(_raycastTransform.position, _raycastTransform.position + (new Vector3(_checkDirection.Value, 0f, 0f).normalized*_maxDist.Value), _enemyLayers.value);
+    //    //if (hit)
+    //    //{
+    //    //    var go = (hit.collider.attachedRigidbody) ? hit.collider.attachedRigidbody.gameObject : hit.collider.gameObject;
+    //    //    Unit unit = go.GetComponent<Unit>();
+    //    //}
+
+    //    RaycastHit2D[] hits;
+    //    hits = Physics2D.LinecastAll(_raycastTransform.position, _raycastTransform.position + (new Vector3(_checkDirection.Value, 0f, 0f).normalized * _maxDist.Value), _enemyLayers.value);
+    //    if (hits.Length > 0)
+    //    {
+    //        foreach (var h in hits)
+    //        {
+    //            var go = (h.collider.attachedRigidbody) ? h.collider.attachedRigidbody.gameObject : h.collider.gameObject;
+    //            Unit unit = go.GetComponent<Unit>();
+    //            if(unit)
+    //            {
+    //                if(unit.OwnerNumber != ownerNumber.Value)
+    //                {
+    //                    // This doesn't guaruntee its the closest.
+    //                    _closestEnemyUnitVar.Value = go;
+    //                    _distanceVar.Value = h.distance;
+    //                    break;
+    //                }
+    //            }
+    //        }
+    //    }
+
+    //}
+}
