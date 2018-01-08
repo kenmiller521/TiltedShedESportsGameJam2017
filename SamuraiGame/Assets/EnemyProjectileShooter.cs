6cdf3c27cb7b853d0b3f39c7f932b1d9d27bcf51
Alex Pinedo <31834217+rocketmanhs@users.noreply.github.com>
2018-01-07 22:41:26 -0800

    fixed changes to projectile script

--- SamuraiGame/Assets/EnemyProjectileShooter.cs
+++ SamuraiGame/Assets/EnemyProjectileShooter.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProjectileShooter : MonoBehaviour
+public class EnemyProjectileShooter : MonoBehaviour
 {
     [SerializeField] private GameObject _projectilePrefab;
     [SerializeField] private Vector2 _shootVector;
     [SerializeField] private float _shootForce;
+    public Collider2D collider;
+
+    private void Update()
+    {
+
+    }
 
     public void ShootProjectile()
     {
@@ -14,10 +20,28 @@ public class ProjectileShooter : MonoBehaviour
         var go = GameObject.Instantiate(_projectilePrefab);
         go.transform.position = this.transform.position;
         var rb = go.GetComponent<Rigidbody2D>();
-        if(rb)
+        if (rb)
         {
             rb.AddForce(_shootVector.normalized * _shootForce, ForceMode2D.Impulse);
         }
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var unit = collision.attachedRigidbody.GetComponent<Unit>();
+        if (unit)
+        {
+            if (unit.OwnerNumber != myOwner)
+            {
 
+            }
+        }
+        ShootProjectile();
+        StartCoroutine(ShootDelay());
+
+    }
+    IEnumerator ShootDelay()
+    {
+        //booolean to set true if it can shoot
+        yield return new WaitForSeconds(1);
+    }
 }
