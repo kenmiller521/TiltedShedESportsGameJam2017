1105d41bd64bbe540107b8490ca4061846d3a722
Michaelwolf95 <michaelwolf95@gmail.com>
2018-01-07 21:08:10 -0800

    Archer is about done.

---
+++ SamuraiGame/Assets/_Scripts/RotationTrajectorySync.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationTrajectorySync : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start ()
+    {
+        if (!rb) rb = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(rb)
+        {
+            Vector3 diff = rb.velocity.normalized;
+            float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, rot_z);
+        }
+	}
+}
