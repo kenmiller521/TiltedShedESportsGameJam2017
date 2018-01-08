2ab572fb84697420775031872d22a4fc5d9c2cc6
Alex Pinedo <31834217+rocketmanhs@users.noreply.github.com>
2018-01-07 22:28:14 -0800

    Arrow halp pleese

--- SamuraiGame/Assets/_Scripts/UnitSystem/ProjectileShooter.cs
+++ SamuraiGame/Assets/_Scripts/UnitSystem/ProjectileShooter.cs
@@ -7,6 +7,14 @@ public class ProjectileShooter : MonoBehaviour
     [SerializeField] private GameObject _projectilePrefab;
     [SerializeField] private Vector2 _shootVector;
     [SerializeField] private float _shootForce;
+    public Collider2D collider;
+
+    private void Update()
+    {
+        OnTriggerEnter2D(collider);
+        StartCoroutine(ShootDelay());
+         
+    }
 
     public void ShootProjectile()
     {
@@ -18,5 +26,17 @@ public class ProjectileShooter : MonoBehaviour
         {
             rb.AddForce(_shootVector.normalized * _shootForce, ForceMode2D.Impulse);
         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        ShootProjectile();
+
+    }
+    IEnumerator ShootDelay()
+    {
+        _projectilePrefab.SetActive(false);
+        yield return new WaitForSeconds(1);
+        Destroy(_projectilePrefab);
+        _projectilePrefab.SetActive(true);
     }
 }
